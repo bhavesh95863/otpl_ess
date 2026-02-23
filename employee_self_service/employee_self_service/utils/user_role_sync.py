97a1986ad26129c037d944e6b30a6c77a5eec9ec
bhavesh95863 <bhavesh.freelancer95863@gmail.com>
2026-02-23 21:51:46 +0000

    fix: update employee after user update

--- employee_self_service/employee_self_service/utils/user_role_sync.py
+++ employee_self_service/employee_self_service/utils/user_role_sync.py
@@ -27,19 +27,8 @@ def sync_employee_fields_from_user_roles(doc, method):
 	is_team_leader = 1 if "TEAM LEADER" in user_roles else 0
 	no_check_in = 1 if "No Check In" in user_roles else 0
 	show_sales_order = 1 if "Show Sales Order" in user_roles else 0
-	
-	# Update employee fields
-	frappe.db.set_value("Employee", employee, {
-		"is_team_leader": is_team_leader,
-		"no_check_in": no_check_in,
-		"show_sales_order": show_sales_order
-	}, update_modified=False)
-	
-	frappe.db.commit()
-	
-	# Log the changes
-	frappe.logger().info(
-		"Updated Employee {0}: is_team_leader={1}, no_check_in={2}, show_sales_order={3}".format(
-			employee, is_team_leader, no_check_in, show_sales_order
-		)
-	)
+	employee_doc = frappe.get_doc("Employee", employee)
+	employee_doc.is_team_leader = is_team_leader
+	employee_doc.no_check_in = no_check_in
+	employee_doc.show_sales_order = show_sales_order
+	employee_doc.save(ignore_permissions=True)
